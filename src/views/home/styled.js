63cdd1b9433a9b121206e5c681e45725b32a5b33
pengliang <624151713@qq.com>
2020-09-10 00:19:05 +0800

    init my react

---
+++ src/views/home/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const HeaderContainer = styled.div`
+  .ant-layout {
+    height: calc(100vh - 64px);
+  }
+
+  .logo,
+  .user {
+    color: #fff;
+  }
+`
+export { HeaderContainer }
